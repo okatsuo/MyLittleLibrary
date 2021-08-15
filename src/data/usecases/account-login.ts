b5f0fa2a8b6475420b5d7354960fc9ed1210ebbe
okatsuo <rafael.goncanna@gmail.com>
2021-08-14 21:59:28 -0300

    feat: ensure AccountLogin work correctly

---
+++ src/data/usecases/account-login.ts
@@ -0,0 +1,38 @@
+import { IAccountModel } from '../../domain/models/account'
+import { ILoginModel } from '../../domain/models/login'
+import { IAccountLogin, ILogin } from '../../domain/usecases/login'
+
+export interface ILoadAccountByEmail {
+  loadAccountByEmail: (email: string) => Promise<IAccountModel>
+}
+
+export interface IHashComparer {
+  compare: (value: string, hashed_value: string) => Promise<boolean>
+}
+
+export interface IGenerateAccessToken {
+  generate: (id: string, name: string) => Promise<string>
+}
+
+export class AccountLogin implements IAccountLogin {
+  constructor (
+    private readonly accountRepository: ILoadAccountByEmail,
+    private readonly hashComparer: IHashComparer,
+    private readonly acessToken: IGenerateAccessToken
+  ) {}
+
+  async login (accountData: ILogin): Promise<ILoginModel> {
+    const account = await this.accountRepository.loadAccountByEmail((accountData.email))
+    if (account) {
+      const isValidPassword = await this.hashComparer.compare(accountData.password, account.password)
+      if (isValidPassword) {
+        const token = await this.acessToken.generate(account.id, account.name)
+        return {
+          token,
+          user: account
+        }
+      }
+    }
+    throw new Error('email or password incorrect')
+  }
+}
